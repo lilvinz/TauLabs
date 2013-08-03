6b5a684c07f3a43c57e330bb0775f54b5c52fa17
lilvinz <lilvinz@nscit.com>
2013-08-04 01:49:01 +0200

    freertos+trace: added compat fix to FreeRTOS v7.4.x

--- flight/PiOS/Common/Libraries/FreeRTOS-Plus-Trace/Include/trcKernelPort.h
+++ flight/PiOS/Common/Libraries/FreeRTOS-Plus-Trace/Include/trcKernelPort.h
@@ -400,7 +400,7 @@ void* prvTraceGetCurrentTaskHandle(void);
 /* Called on each OS tick. Will call uiPortGetTimestamp to make sure it is called at least once every OS tick. */
 #undef traceTASK_INCREMENT_TICK
 #define traceTASK_INCREMENT_TICK( xTickCount ) \
-    if (uxSchedulerSuspended == ( unsigned portBASE_TYPE ) pdTRUE || uxPendedTicks == 0) { trcKERNEL_HOOKS_INCREMENT_TICK(); } \
+    if (uxSchedulerSuspended == ( unsigned portBASE_TYPE ) pdTRUE || uxMissedTicks == 0) { trcKERNEL_HOOKS_INCREMENT_TICK(); } \
 	if (uxSchedulerSuspended == ( unsigned portBASE_TYPE ) pdFALSE) { trcKERNEL_HOOKS_NEW_TIME(DIV_NEW_TIME, xTickCount + 1); }
 
 /* Called on each task-switch */
